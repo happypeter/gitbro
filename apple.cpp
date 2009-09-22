10ca871afe3cf9bc6cb33de9668e6981489e1678
Peter King <happypeter1983@gmail.com>
2009-09-22 09:59:57 +0800

    now the error is showColor() undifined

--- apple.cpp
+++ apple.cpp
@@ -9,13 +9,12 @@ apple::~apple()
 cout<<"I am the destructor<<endl";
 }
 
-int apple:: showColor()
+int apple::showColor()
 {
-
 cout<<"the apple color is:"<<color<<endl;
-
+return 0;
 }
-int apple:: setColor()
+int apple::setColor()
 {
 color = "red";
 cout<<"set color to red"<<endl;
