61dd1785ae06e778ba961c58aeb55c75f0343e52
peter <peter@peter-dt.(none)>
2009-10-07 15:28:24 +0800

    ui is alive

--- main.cpp
+++ main.cpp
@@ -1,11 +1,16 @@
 #include <QApplication>
 #include <iostream>
 #include "reader.h"
+#include "ui_dialog.h"
 using namespace std;
 
 int main(int argc, char *argv[])
 {
 	QApplication app(argc, argv);
+	QWidget *widget = new QWidget;
+	Ui_Dialog ui;
+	ui.setupUi(widget);
+	widget->show();
 	reader re;
 	re.showColor();
 	return app.exec();
