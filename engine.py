23f8a1e647d38ad75e5084ba18a620ceb7053e6d
Peter Wang <happypeter1983@gmail.com>
2010-01-22 11:00:38 +0800

    ok now we passed the git-info.txt, but not do anything useful yet

--- engine.py
+++ engine.py
@@ -16,4 +16,15 @@ file_name = "out"
 git_cmd = "git log --follow "+file_name
 cmd = git_cmd+">"+git_info_file
 os.system(cmd)
+#actually we only need all the commit hash num
+#so now we will read git-info.txt, get all the commit number and save them in a list
+#and I think I need a function to do this for me
 
+def get_commit_hashes(file_name):
+    print "I am working on "+file_name
+    list = [1,2,3]
+    return list
+
+commit_list = get_commit_hashes(git_info_file)
+
+print commit_list
