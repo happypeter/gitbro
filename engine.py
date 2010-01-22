9b3468720fecfccad2a8de4c615d56de9971457f
Peter Wang <happypeter1983@gmail.com>
2010-01-22 10:26:27 +0800

    change cwd to the repo in concern

--- engine.py
+++ engine.py
@@ -5,3 +5,9 @@ if os.path.exists(repo_path): #output files go here
     print "we are going to check "+repo_path 
 else:
     print "can not find the repo"    
+
+cwd = os.getcwd()
+print cwd
+os.chdir(repo_path)
+cwd = os.getcwd()
+print cwd
