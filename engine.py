41a770f0af06f709f37e641d749d4b19c6afc004
Peter Wang <happypeter1983@gmail.com>
2010-01-29 09:28:19 +0800

    style modification according to PEP8

--- engine.py
+++ engine.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python
-import os,sys
-if len(sys.argv)!=2:
-   print """
-gitbro: missing operand
+import os
+import sys
+if len(sys.argv) != 2:
+   print """gitbro: missing operand
 Usage:
       $gitbro filename(no parent dir is allowed within the filename)
           """
@@ -12,7 +12,7 @@ file_name = sys.argv[1] # this is the file we are working on
 #FIXME, the following code has hole.
 #what if $gitbro my-git-repo/file
 ### check if in a git repo --begin
-cmd="git log>/dev/null"
+cmd = "git log>/dev/null"
 if not os.system(cmd):
     print "OK, we are in a repo, Let's get to work..."
 else:
@@ -20,7 +20,7 @@ else:
     sys.exit(1)
 ### check if in a git repo --end
 home_dir = os.path.expanduser("~")#"/home/peter" in my case
-output_path = home_dir +"/"+".gitbro/"
+output_path = home_dir + "/" +".gitbro/"
 patch_dir = "patch/" 
 #output_path is the dir we store all the output files
 #including tmp files like git-info.txt and useful final output
@@ -28,26 +28,24 @@ patch_dir = "patch/"
 #patch configurable
 
 if os.path.exists(output_path): #output files go here
-    os.system("rm -rf "+output_path)
+    os.system("rm -rf " + output_path)
 
 print "make dir..."
-os.system("mkdir -p "+output_path+patch_dir)
+os.system("mkdir -p " + output_path+patch_dir)
 
  
 git_info_file = output_path + "git-info.txt"
-git_cmd = "git log --follow "+file_name+"|grep commit"
+git_cmd = "git log --follow " + file_name + "|grep commit"
 print git_cmd
-cmd = git_cmd+">"+git_info_file
+cmd = git_cmd + ">" + git_info_file
 os.system(cmd)
 
 def get_commit_hashes(file_name):
-    """
-actually we only need all the commit hash num
-so now we will read git-info.txt
+    """read git-info.txt
 get all the commit number and save them in a list
 
     """
-    f=open(file_name,"r");
+    f = open(file_name,"r");
     done = 0
     commit_hash_list = []
     while not done:
@@ -65,13 +63,13 @@ commit_list = get_commit_hashes(git_info_file)
 commit_list.reverse()
 #we reverse the list since we want the first commit first
 #so that we can generate the first patch first, look shown below
-n=0
+n = 0
 for commit in commit_list:
-    git_cmd = "git show "+commit+' '+file_name
+    git_cmd = "git show " + commit + ' ' + file_name
     patch_file_name = output_path + patch_dir +  file_name + "-" + str(n) + ".diff"
     cmd = git_cmd + ">" + patch_file_name
     os.system(cmd)
-    n=n+1
+    n= n+1
 
 
 
