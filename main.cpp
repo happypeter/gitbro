a154b209ad9c309cf80bc3e62d37d615c9f91d81
Peter Wang <happypeter1983@gmail.com>
2010-02-05 15:01:18 +0800

    doxygen comment format

--- main.cpp
+++ main.cpp
@@ -2,7 +2,13 @@
 #include <iostream>
 #include "reader.h"
 using namespace std;
+/*!
+@brief main of all
 
+the des of main goes here
+
+@param nIndex 0: Update, 1:Install, 2:Erase
+*/
 int main(int argc, char *argv[])
 {
 	QApplication app(argc, argv);
