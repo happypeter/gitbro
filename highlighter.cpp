e45d3c41ccc8a7ba5ca8676e5d9932539bdb2989
Peter Wang <happypeter1983@gmail.com>
2010-02-06 15:30:09 +0800

    + is catched

--- highlighter.cpp
+++ highlighter.cpp
@@ -7,7 +7,13 @@ Highlighter::Highlighter(QTextDocument *parent)
   HighlightingRule rule;
 
   singleLineCommentFormat.setBackground(QColor("#99CC66"));
-  rule.pattern = QRegExp("//[^\n]*");
+  // http://doc.trolltech.com/4.6/qregexp.html
+  rule.pattern = QRegExp(QRegExp::escape("+")+"[^\n]*");
+  //now it works, but the problem is:1) it matches "+" at anyposition,
+  //I actully only want to find a line start with "+"; 2)I should not
+  //set the backgroud of the text, it is ugly, I should set background
+  // of the whole line 
+
   rule.format = singleLineCommentFormat;
   highlightingRules.append(rule);
 }
