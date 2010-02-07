a663d856c2756b6fdfbd1c2b0825c29f8807cf7e
Peter Wang <happypeter1983@gmail.com>
2010-02-07 16:07:41 +0800

    it is not so flexiable to pass file name this way, we need to think another way

--- reader/main.cpp
+++ reader/main.cpp
@@ -13,11 +13,6 @@ int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
     Reader reader;
-    QStringList args = app.arguments();
-    args.removeFirst(); // program name.. 
-    QString filename = args.join(" ");
-    cout<<filename.length()<<endl;//see qt-creator/src/tools/texteditor/
-    reader.openFile(filename);    // to know how to handle files
     reader.resize(640, 512);
     reader.show();
     return app.exec();
