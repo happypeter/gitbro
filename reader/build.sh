e4e50cbebd1f914a5792c6f2e8d0382898e04bcf
Peter Wang <happypeter1983@gmail.com>
2010-02-07 20:04:39 +0800

    automation is gooood

--- reader/build.sh
+++ reader/build.sh
@@ -3,3 +3,13 @@ make clean
 qmake -project
 qmake 
 make
+make clean
+echo "you want to run it? [y] or [n]?"
+read h
+ if [ "$h" = "y" ]; then
+#Beaware of the spaces above
+./reader
+else
+echo bye
+fi
+
