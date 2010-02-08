6634b2972526c005839bc8ef8115b3b9e45897bb
Peter Wang <happypeter1983@gmail.com>
2010-02-08 19:31:13 +0800

    it runs, but nothing shows up on the frame

--- network/screen.h
+++ network/screen.h
@@ -8,12 +8,6 @@
 #include <QRect>
 #include <QString>
 
-#define FrameWidth          3 
-#define Step                5  
-#define BaseFontHeight      10
-#define BaseLineLenght      5
-#define SpaceMargin         5
-
 class Screen : public QFrame
 {
     Q_OBJECT
@@ -21,36 +15,14 @@ class Screen : public QFrame
 public:
     Screen( QWidget *parent = 0 );
    
-    void animate( double y );
-    void set_animate( bool b );
-    void setXTitle( QString str );
-    void setYTitle( QString str );
-    void drawCurve();    
-    void refreshPixmap();	   
 protected:
-    void initNumber(QPainter &painter);
     void drawCordinate(QPainter &painter);
-//    virtual void showEvent ( QShowEvent * );
-//    virtual void hideEvent ( QHideEvent * );
      void paintEvent( QPaintEvent * );
      void resizeEvent( QResizeEvent * );
-     QSize minimumSizeHint() const;
 	
 private:  
-    double Yold, Ynew;
-    int numXTicks, numYTicks;
-    bool firstShow, m_animate;
     QPixmap newPixmap,savePixmap,midPixmap;
-    int sec, min, hour;
-    QRect rectCordinate;
-    QRect fromSaveRect;
-    QRect toNewRect;
-    QRect rectXText;
-    QRect rectYText;
-    QVector<double> Yval;
     QPainter painter;
-    QString stringYTitle;
-    QString stringXTitle;
 };
 
 
