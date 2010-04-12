bfa7b7a0d015f4af1f83af0898078731a5c4db6c
Peter Wang <happypeter1983@gmail.com>
2010-04-12 21:13:45 +0800

    git.h

---
+++ reader/git.h
@@ -0,0 +1,21 @@
+#ifndef GIT_H
+#define GIT_H
+
+#include <QWidget>
+
+class Git : public QWidget
+{
+    Q_OBJECT
+
+public:                                                                       
+    Git();
+    bool isFileInRepo(QString);
+    void startGit(QString);
+
+public slots:
+
+private:
+
+};
+
+#endif // GIT_H
