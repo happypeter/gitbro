ee44a727eb60e74d2fcbbd84d36c11adf722d0d7
Peter Wang <happypeter1983@gmail.com>
2010-04-15 20:07:34 +0800

    TUI way to do qt doc lookup

---
+++ wiki/qt/QT-doc/qref.sh
@@ -0,0 +1,21 @@
+#!/bin/bash          
+
+# I suppose you have all the qt-doc pages in /var/www
+cd /var/www
+kw=$1".html" #concating two string is easy
+name=$(ls|grep -i $kw) #no space is allowed between "name" and "="
+echo --------
+echo $name
+
+lynx $name
+
+#put me in $PATH and use me this way
+#peter@cow:~$ qref QString 
+
+#####            BUG          ####################
+#
+#  you have to give me the full name of the class
+#  I do not care about the case, but if you give me
+#  sth like "QStr", I will reply you with shit
+#
+#################################################
