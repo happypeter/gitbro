3ed1f2783bb181744a1fcfb091351e357c24ff00
Peter Wang <happypeter1983@gmail.com>
2010-04-16 11:11:26 +0800

    use delete for all

--- wiki/oop.cpp
+++ wiki/oop.cpp
@@ -13,4 +13,12 @@ the storage will be freeed, when the execution reaches the ending `}`. But the `
 does not follow that rule, `Thinking in C++` tells me you need to be responsible to delete the objects, 
 but I never did so, but my programs just run so well, why? 
 
-A: 
+A: this is a issue of grabage collection, below is what I found, and it confused me more:-)
+
+http://en.wikipedia.org/wiki/C%2B%2B#Criticism
+	Some have criticized C++ for not having garbage collection, Dr. Stroustrup even discusses this on his website, he states that the memory management capabilities of C++ essentially perform the same function as garbage collection without an implicit garbage collector.
+
+http://en.wikipedia.org/wiki/Delete_(C%2B%2B)
+	A call to delete must be made for every call to new to avoid a memory leak.
+
+so the above two links convined me that I will be better off if I can use delete for all newed items, it is just safer.
