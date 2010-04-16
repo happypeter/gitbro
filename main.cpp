8ce54a2f829e212c70d782e8f148f8e36abfc222
Peter Wang <happypeter1983@gmail.com>
2010-04-16 14:03:37 +0800

    error related to pro file
    undefined reference to `QMyWidget::QMyWidget()'
    shows up all the time
    and it takes me quit long to realize
    I need to run `qmake -project' to update my pro file
    whenever I add new files

--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
 #include "dialog.h"
-
+#include "parent.h"
 
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
     DialogForm df;
+    QMyWidget my;
+    my.show();
     df.show();
     return app.exec();
 
---
+++ parent.cpp
@@ -0,0 +1,8 @@
+#include "parent.h"
+#include <QPushButton>
+QMyWidget::QMyWidget()
+{
+    QPushButton * button=new QPushButton(this);
+}
+
+
