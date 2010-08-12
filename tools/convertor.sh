32261228792f350e4cdfec7de15a949695f63f91
Peter Wang <happypeter1983@gmail.com>
2010-08-12 10:49:50 +0800

    comment matters

--- tools/convertor.sh
+++ tools/convertor.sh
@@ -1,12 +1,29 @@
 #!/bin/bash -x
 
+################################
+#
+#    Variables
+#    
+#################################
 REPO_DIR=$HOME/website-gitbro
 POST_DIR=$REPO_DIR/posts
 OUTPUT_DIR=$HOME/ooo
 HEADER=$REPO_DIR/css/header.html
 FOOTER=$REPO_DIR/css/footer.html
+
+################################
+#
+#    Update rudely
+#    
+#################################
 rm -rf $OUTPUT_DIR
 mkdir $OUTPUT_DIR
+
+################################
+#
+#    start Converting
+#    
+#################################
 cd $POST_DIR
 for file in `ls .`
 do
