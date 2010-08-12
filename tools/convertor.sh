0272060e295d1d31cf1d0c578b8c6c3ec39e8413
Peter Wang <happypeter1983@gmail.com>
2010-08-12 10:21:13 +0800

    with "-x" set in the script, we do not need these

--- tools/convertor.sh
+++ tools/convertor.sh
@@ -2,27 +2,14 @@
 
 REPO_DIR=$HOME/website-gitbro
 POST_DIR=$REPO_DIR/posts
-echo $POST_DIR
 OUTPUT_DIR=$HOME/gitbro-output
 HEADER=$REPO_DIR/css/header.html
 FOOTER=$REPO_DIR/css/footer.html
 rm -rf $OUTPUT_DIR
 mkdir $OUTPUT_DIR
 cd $POST_DIR
-echo
-echo 1-------
-echo $POST_DIR
-pwd
-echo 1^^^^^^^
-echo
 for file in `ls .`
 do
-    echo
-    echo 2--------
-    echo $PWD
-    echo $file
-    echo 2^^^^^^^^
-    echo
     markdown $file>$file".html"
     mv *.html $OUTPUT_DIR
 done
