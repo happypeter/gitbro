8f74bf493015c75e48fcfd5406dba09f9ecaee25
Peter Wang <happypeter1983@gmail.com>
2010-08-12 10:34:34 +0800

    a easier OUTPUT_DIR

--- tools/convertor.sh
+++ tools/convertor.sh
@@ -2,7 +2,7 @@
 
 REPO_DIR=$HOME/website-gitbro
 POST_DIR=$REPO_DIR/posts
-OUTPUT_DIR=$HOME/gitbro-output
+OUTPUT_DIR=$HOME/ooo
 HEADER=$REPO_DIR/css/header.html
 FOOTER=$REPO_DIR/css/footer.html
 rm -rf $OUTPUT_DIR
@@ -10,7 +10,10 @@ mkdir $OUTPUT_DIR
 cd $POST_DIR
 for file in `ls .`
 do
-    markdown $file>$file".html"
+    markdown $file>$file".html" 
+    ## we should not have file prefix for posts
+    ## other wise we need to use filebasename here
+
     mv *.html $OUTPUT_DIR
 done
 
