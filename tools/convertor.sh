04da68137fda5ba629df8f1554a93a12039b102c
Peter Wang <happypeter1983@gmail.com>
2010-08-23 11:36:15 +0800

    add pageTitle to all_posts.html

--- tools/convertor.sh
+++ tools/convertor.sh
@@ -26,8 +26,9 @@ mkdir $OUTPUT_DIR
 #    
 #################################
 cd $POST_DIR
-stat * -c %y" "%n|sort -r|awk -F" " '{print "<h2>"$1" ""<a href="$4".html"">"$4"</a>""</h1>"}'>all_posts.html.tmp
-cat $HEADER all_posts.html.tmp > all_posts.html
+stat * -c %y" "%n|sort -r|awk -F" " '{print "<h3>"$1" ""<a href="$4".html"">"$4"</a>""</h3>"}'>all_posts.html.tmp
+echo "<br /><br /><center><h1>All Posts</h2></center><br />">pageTitle.tmp
+cat $HEADER pageTitle.tmp all_posts.html.tmp > all_posts.html
 rm *.tmp
 mv all_posts.html $OUTPUT_DIR
 #################################
