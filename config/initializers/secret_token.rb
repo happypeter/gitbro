e542b06747435ab86fc15ffc2377ea18283c82c9
Peter Wang <happypeter1983@gmail.com>
2011-10-02 16:22:46 +0800

    i

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GritTest::Application.config.secret_token = '13f103ff33ee32b1bd02422c08642c010083d1c2ac54610e309736296578b8ebedef256368d016f716cc0a98a413ee3bfcb1d2a745070e2c664201902af9d612'
