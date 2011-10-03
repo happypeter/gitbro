a8aa8ceabf8e9e3dffb9a7039e011ac14a25c174
Peter Wang <happypeter1983@gmail.com>
2011-10-03 15:59:46 +0800

    sessions_controller.rb copied

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,21 @@
-class SessionsController < ApplicationController
-end
+class SessionsController < ApplicationController  
+  def new  
+  end  
+    
+  def create  
+  # called when "post sessions"
+    user = User.authenticate(params[:email], params[:password])  
+    if user  
+      session[:user_id] = user.id # the session here, has nothing to do with the controller name Sessions, I will test this by using another controller name tommorrow! 
+      redirect_to root_url, :notice => "Logged in!"  
+    else  
+      flash.now.alert = "Invalid email or password"  
+      render "new" # new => ../views/sessions/new.html.er 
+    end  
+  end  
+
+  def destroy  
+    session[:user_id] = nil  
+    redirect_to root_url, :notice => "Hi, Logged out!"  
+  end  
+end  
