3670fdf0ba3891148c49c296b36cadfb5c65bf9e
Peter Wang <happypeter1983@gmail.com>
2011-10-03 16:04:59 +0800

    application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,21 @@
-class ApplicationController < ActionController::Base
-  protect_from_forgery
-end
+class ApplicationController < ActionController::Base  
+  protect_from_forgery  
+  helper_method :current_user  
+    
+  private  
+  def current_user  
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]  
+    # session[:user_id] will stay there even auth_love and its db is deleted
+    # session[:user_id] will still be valid, when auth_love is started again
+    # it can only be removed by close the firefox tab and then restart
+    # firefox. so that is what they called cookie, I think.
+  end  
+end  
+#http://stackoverflow.com/questions/3549713/controller-helper-method
+#helper_method is useful when the functionality is something that's used between both the controller and the view. A good example is something like current_user.
+
+#If the method deals more with controller logic and not formatting then it belongs in the controller. Something like current_user would be shared between all controllers so it should be defined in the ApplicationController.
+
+#True "helper" methods deal with the view and handle things like formatting and template logic. These are seldom needed in the controller and they belong in their own module under app/helpers. You can include these in your controller when needed, but you end up with the whole module worth of view helper methods available to your controller.
+
+
