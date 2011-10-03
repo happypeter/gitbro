3d68bede6392532494faeb1fc87bcdde52484568
Peter Wang <happypeter1983@gmail.com>
2011-10-03 16:43:22 +0800

    schema changed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111003074451) do
+ActiveRecord::Schema.define(:version => 20111003085356) do
 
   create_table "posts", :force => true do |t|
     t.string   "name"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(:version => 20111003074451) do
     t.string   "password_salt"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "name"
   end
 
 end
