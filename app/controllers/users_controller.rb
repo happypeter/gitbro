ec3358cae2b25dcfd10e9928dd2bbb4dbfa28fc6
Peter Wang <happypeter1983@gmail.com>
2011-10-03 16:13:08 +0800

    users_controller.rb copied

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,18 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
-
+   
+  def create  
+ # this method is called when you click the "created user" button, then I see
+ # in Firebug a "post users" request, so according to 
+ # http://guides.rubyonrails.org/routing.html
+ # the users#create will be called
+    @user = User.new(params[:user])  
+    if @user.save  
+      redirect_to root_url, :notice => "Signed up!"  
+    else  
+      render "new"  
+    end  
+  end  
 end
